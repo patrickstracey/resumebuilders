1fb0b129b7b50b07d15b1b593128027c9dfa8ece
patrickstracey <pstracey17@gmail.com>
2021-02-14 18:45:10 -0800

    mockup category filter btns

--- src/components/jobs/JobIndex.vue
+++ src/components/jobs/JobIndex.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="opp-holder" class="flex-column">
+    <div class="flex-row" id="filter-btn-holder">
+      <button
+        v-for="category of categories"
+        v-bind:key="category.id"
+        class="link filter-btn"
+        @click="filterListing(category.id)"
+      >
+        {{ categoriesByInt[category.id] }}
+      </button>
+    </div>
+
     <job-card
       v-for="job of jobs"
       v-bind:key="job.id"
@@ -11,6 +22,8 @@
 <script>
 import JobCard from "./JobCard.vue";
 import firebaseInit from "../../firebaseInit.js";
+import { CATEGORIES, CATEGORIES_BY_INT } from "../../enums/category.js";
+
 const jobsRef = firebaseInit.firestore().collection("opportunities");
 
 export default {
@@ -19,6 +32,8 @@ export default {
   data() {
     return {
       jobs: [],
+      categories: CATEGORIES,
+      categoriesByInt: CATEGORIES_BY_INT,
     };
   },
   methods: {
@@ -28,6 +43,11 @@ export default {
         const jawb = { id: result.id, ...result.data() };
         this.jobs.push(jawb);
       });
+    },
+    filterListing(category) {
+      console.log(
+        `filtering for ${this.categoriesByInt[category]} -- ${category}`
+      );
     },
   },
   mounted() {
@@ -44,5 +64,23 @@ export default {
   margin-top: 40px;
   margin-left: auto;
   margin-right: auto;
+}
+
+#filter-btn-holder {
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+.filter-btn {
+  margin: 4px;
+  background-color: white;
+  color: var(--rb-main);
+  border-width: 3px;
+  flex-basis: 238px;
+  flex-grow: 1;
+}
+
+.filter-btn:hover {
+  background-color: var(--rb-main-light-active);
 }
 </style>
