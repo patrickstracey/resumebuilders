7c2d6ec795984d4369bdbe164968f902d03bb89c
patrickstracey <pstracey17@gmail.com>
2021-02-20 14:20:19 -0800

    init vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
+import { createStore } from "vuex";
 
 import App from "./App.vue";
 import TheHomePage from "./components/index/TheHomePage.vue";
@@ -16,6 +17,44 @@ const router = createRouter({
   ],
 });
 
+const store = createStore({
+  state() {
+    return {
+      previousResults: {
+        0: [],
+      },
+    };
+  },
+  mutations: {
+    addListingToState(state, payload) {
+      const filterGroup = state.previousResults[payload.filterId];
+      if (filterGroup) {
+        if (filterGroup.includes(payload.listing)) {
+          return;
+        } else {
+          state.previousResults[payload.filterId].push(payload.listing);
+        }
+      } else {
+        if (payload.listing) {
+          state.previousResults[payload.filterId] = [payload.listing];
+        } else {
+          state.previousResults[payload.filterId] = [];
+        }
+      }
+    },
+  },
+  getters: {
+    retrieveStoredResults: (state) => (filterID) => {
+      if (state.previousResults[filterID]) {
+        return state.previousResults[filterID];
+      } else {
+        return null;
+      }
+    },
+  },
+});
+
 createApp(App)
   .use(router)
+  .use(store)
   .mount("#app");
