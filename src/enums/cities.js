8b0a59bafcefd8459aa46a67c480d14db8ce151a
patrickstracey <pstracey17@gmail.com>
2021-02-27 11:05:12 -0800

    add geopoints to jobs

---
+++ src/enums/cities.js
@@ -0,0 +1,32 @@
+export const CITIES = {
+  "Austin, TX": {
+    name: "Austin, TX",
+    lat: 30.266666,
+    long: -97.73333,
+  },
+  "Chicago, IL": {
+    name: "Chicago, IL",
+    lat: 41.881832,
+    long: -87.623177,
+  },
+  "Los Angeles, CA": {
+    name: "Los Angeles, CA",
+    lat: 34.052235,
+    long: -118.243683,
+  },
+  "New York, NY": {
+    name: "New York, NY",
+    lat: 40.73061,
+    long: -73.935242,
+  },
+  "Reno, NV": {
+    name: "Reno, NV",
+    lat: 39.5296,
+    long: -119.8138,
+  },
+  "Seattle, WA": {
+    name: "Seattle, WA",
+    lat: 47.608013,
+    long: -122.335167,
+  },
+};
