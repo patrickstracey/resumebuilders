2ac88d4cdadff3be868422cbc55130fedc814f33
patrickstracey <pstracey17@gmail.com>
2021-03-07 09:34:40 -0800

    update all 3 enums

--- src/enums/jobTypes.js
+++ src/enums/jobTypes.js
@@ -1,13 +1,13 @@
 export const JOB_TYPES = {
   INTERNSHIP: { id: 1, display: "Internship 🎓" },
   REMOTE: { id: 2, display: "Remote Friendly 🌎" },
-  TEMP: { id: 3, display: "Temporary 🌈" },
+  //TEMP: { id: 3, display: "Temporary 🌈" },
   PART_TIME: { id: 4, display: "Part Time ⌚" },
 };
 
 export const JOB_TYPE_BY_INT = {
   1: "Internship 🎓",
   2: "Remote Friendly 🌎",
-  3: "Temporary 🌈",
+  //3: "Temporary 🌈",
   4: "Part Time ⌚",
 };
